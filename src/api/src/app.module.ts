5c591ab88f7a753740f0cec1255e014eecf5c06a
LeticiaZalasik <leticia.zalasik@premiersoft.net>
2026-08-20 11:45:59 -0300

    Documentação, configurações de banco de dados, docker, typeorm e swagger

--- src/api/src/app.module.ts
+++ src/api/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { dataSourceOptions } from './database/data-source';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsuarioModule } from './usuarios/usuario.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    TypeOrmModule.forRootAsync({
+      useFactory: () => dataSourceOptions,
+    }),
+
+    UsuarioModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
